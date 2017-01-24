005ffeb3677510f6df8ba8651b46e2d24b0d2276
Tyler Huffer <Thuffer86@gmail.com>
2017-01-23 20:49:58 -0600

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+    $("#menu").toggleClass('active');
+	});
+    
+    
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('#nav-icon').toggleClass('open');
+        $("#menu").toggleClass('active');
+        $('.container').removeClass('activePage');
+        target.toggleClass('activePage');
+        return false;
+      }
+    }
+  });
+});
+});
