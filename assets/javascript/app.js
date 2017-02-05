d8fb33ada573ea36a1f02489784217872e8e43fb
Tyler Huffer <Thuffer86@gmail.com>
2017-02-04 18:22:07 -0600

    fixed a thing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,30 @@ $(document).ready(function(){
     $("#menu").toggleClass('active');
 });
     
+  
     
+if ($(window).width() < 768) { 
+    
+    $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('#nav-icon').toggleClass('open');
+        $("#menu").toggleClass('active');
+        $('.container').removeClass('activePage');
+        $('body').removeClass('noScroll');
+        target.toggleClass('activePage');
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1400);
+        return false;
+      }
+    }
+  });
+});
+} else {
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -22,4 +45,5 @@ $(function() {
     }
   });
 });
+}
 });
